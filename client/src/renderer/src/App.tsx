848d7ca28262a4018ae47844a037fc840e36920f
Darshan Heble <darshanheble@gmail.com>
2024-06-01 01:03:53 +0530

    ..

--- client/src/renderer/src/App.tsx
+++ client/src/renderer/src/App.tsx
@@ -1,26 +1,21 @@
-// import { Container, CssBaseline, createTheme } from '@mui/material'
-// import { ThemeProvider } from 'styled-components'
+import { Container, CssBaseline, ThemeProvider, createTheme } from '@mui/material'
 import AppNavBar from './components/AppNavBar'
 import Versions from './components/Versions'
 
-// const darkTheme = createTheme({
-//   palette: {
-//     mode: 'dark'
-//   }
-// })
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark'
+  }
+})
 
 function App(): JSX.Element {
   return (
     <>
-      {/* <ThemeProvider theme={darkTheme}>
+      <ThemeProvider theme={darkTheme}>
         <CssBaseline />
-        <Container className="main" component="main">
-          hello
-        </Container>
-      </ThemeProvider> */}
-      app
-      <AppNavBar />
-      <Versions />
+        <Container className="main" component="main"></Container>
+        <AppNavBar />
+      </ThemeProvider>
     </>
   )
 }
