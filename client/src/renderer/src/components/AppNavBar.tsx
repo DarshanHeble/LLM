0c5606c6e6162b4fe7e7b4404ec554daecc81255
Darshan Heble <darshanheble@gmail.com>
2024-06-06 01:03:02 +0530

    ..

--- client/src/renderer/src/components/AppNavBar.tsx
+++ client/src/renderer/src/components/AppNavBar.tsx
@@ -85,18 +85,6 @@ export default function AppNavBar(): JSX.Element {
                 height: 32,
                 ml: -0.5,
                 mr: 1
-              },
-              '&::before': {
-                content: '""',
-                display: 'block',
-                position: 'absolute',
-                top: 0,
-                right: 14,
-                width: 10,
-                height: 10,
-                bgcolor: 'background.paper',
-                transform: 'translateY(-50%) rotate(45deg)',
-                zIndex: 0
               }
             }}
             elevation={0}
--- client/src/renderer/src/components/layout/CardLayout.tsx
+++ client/src/renderer/src/components/layout/CardLayout.tsx
@@ -1,4 +1,22 @@
-import { Card, CardActionArea, CardContent, Slide, Typography } from '@mui/material'
+/* eslint-disable react/prop-types */
+import {
+  AppBar,
+  Button,
+  Card,
+  CardActionArea,
+  CardContent,
+  Dialog,
+  Divider,
+  IconButton,
+  List,
+  ListItemButton,
+  ListItemText,
+  Slide,
+  Toolbar,
+  Typography
+} from '@mui/material'
+import CloseIcon from '@mui/icons-material/Close'
+import React from 'react'
 import { TransitionProps } from '@mui/material/transitions'
 
 const Transition = React.forwardRef(function Transition(
@@ -9,6 +27,7 @@ const Transition = React.forwardRef(function Transition(
 ) {
   return <Slide direction="up" ref={ref} {...props} />
 })
+
 function CardLayout({ value }): JSX.Element {
   const [open, setOpen] = React.useState(false)
 
@@ -20,32 +39,58 @@ function CardLayout({ value }): JSX.Element {
     setOpen(false)
   }
 
-  const click = (): void => {
-    console.log('hello')
-  }
+  // const click = (): void => {
+  //   console.log('hello')
+  // }
 
   return (
-    <Card
-      sx={{
-        minWidth: 200,
-        minHeight: 200
-      }}
-    >
-      <CardActionArea sx={{ height: '100%' }} onClick={handleClickOpen}>
-        <CardContent
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignContent: 'center',
-            height: '100%'
-          }}
-        >
-          <Typography component="h1" fontSize={40} sx={{ display: 'grid', placeItems: 'center' }}>
-            {value.name}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <>
+      <Card
+        sx={{
+          minWidth: 200,
+          minHeight: 200
+        }}
+      >
+        <CardActionArea sx={{ height: '100%' }} onClick={handleClickOpen}>
+          <CardContent
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignContent: 'center',
+              height: '100%'
+            }}
+          >
+            <Typography component="h1" fontSize={40} sx={{ display: 'grid', placeItems: 'center' }}>
+              {value.name}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+      <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
+        <AppBar sx={{ position: 'relative' }}>
+          <Toolbar>
+            <IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
+              <CloseIcon />
+            </IconButton>
+            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+              Sound
+            </Typography>
+            <Button autoFocus color="inherit" onClick={handleClose}>
+              save
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <List>
+          <ListItemButton>
+            <ListItemText primary="Phone ringtone" secondary="Titania" />
+          </ListItemButton>
+          <Divider />
+          <ListItemButton>
+            <ListItemText primary="Default notification ringtone" secondary="Tethys" />
+          </ListItemButton>
+        </List>
+      </Dialog>
+    </>
   )
 }
 
