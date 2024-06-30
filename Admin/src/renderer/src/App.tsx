3c192a4ad4d8d3aaccd33e2255444cc69e15e4ef
Darshan Heble <darshanheble@gmail.com>
2024-07-01 02:28:29 +0530

    ..

--- Admin/src/renderer/src/App.tsx
+++ Admin/src/renderer/src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
 import DashBoard from './components/pages/DashBoard'
-import { Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import ManageBooks from './components/pages/ManageBooks'
 import ManageUsers from './components/pages/ManageUsers'
 import ViewIssuedBooks from './components/pages/ViewIssuedBooks'
@@ -32,22 +32,24 @@ function App(): JSX.Element {
 
   return (
     <>
-      <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
+      <HashRouter>
+        <ThemeProvider theme={darkTheme}>
+          <CssBaseline />
 
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/forgetPassword" element={<ForgotPassword />} />
-          <Route path="/resetPassword" element={<ResetPassword />} />
-          <Route path="/signUp" element={<SignUp />} />
-          <Route path="/dashBoard" element={<DashBoard />} />
-          <Route path="/manageBooks" element={<ManageBooks />} />
-          <Route path="/manageUsers" element={<ManageUsers />} />
-          <Route path="/issueBook" element={<IssueBook />} />
-          <Route path="/viewIssuedBooks" element={<ViewIssuedBooks />} />
-          <Route path="/returnBooks" element={<ReturnBooks />} />
-        </Routes>
-      </ThemeProvider>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/forgetPassword" element={<ForgotPassword />} />
+            <Route path="/resetPassword" element={<ResetPassword />} />
+            <Route path="/signUp" element={<SignUp />} />
+            <Route path="/dashBoard" element={<DashBoard />} />
+            <Route path="/manageBooks" element={<ManageBooks />} />
+            <Route path="/manageUsers" element={<ManageUsers />} />
+            <Route path="/issueBook" element={<IssueBook />} />
+            <Route path="/viewIssuedBooks" element={<ViewIssuedBooks />} />
+            <Route path="/returnBooks" element={<ReturnBooks />} />
+          </Routes>
+        </ThemeProvider>
+      </HashRouter>
     </>
   )
 }
